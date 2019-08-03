88945e13b1544094ea5c20311bc5c6b638101c34
kobira-kk <foxmancoffee@yahoo.co.jp>
2019-08-03 16:58:13 +0900

    first Commit

---
+++ MarubatuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarubatuApp
+//
+//  Created by 小平恭兵 on 2019/08/03.
+//  Copyright © 2019 kk-project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
